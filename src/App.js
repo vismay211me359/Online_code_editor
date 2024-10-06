31190019581eb4609a854a72d4af2943c373fa6b
vismay211me359 <vismayms10@gmail.com>
2024-10-06 23:27:58 +0530

    frontend complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import EditorBox from './components/EditorBox';
+import CompileBox from './components/CompileBox';
+import Navbar from './components/Navbar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [lang, setLang] = useState("javascript");
+    const [theme, setTheme] = useState("vs-dark");
+    const [fontSize, setFontSize] = useState(15);
+    const [userCode, setUserCode] = useState("");
+    const [stdin, setStdin] = useState("");
+    const [stdout, setStdout] = useState("");
+
+    const versionIdList = {
+        "javascript": "4",
+        "python": "4"
+    };
+
+    const langCodeList = {
+        "javascript": "nodejs",
+        "python": "python3", 
+    };
+
+    const handleRun = () => {
+
+
+    }
+
+    return (
+        <div className="App">
+            <Navbar
+                className="navbar"
+                lang={lang} setLang={setLang}
+                theme={theme} setTheme={setTheme}
+                fontSize={fontSize} setFontSize={setFontSize}
+                handleRun={handleRun}
+            />
+            <div className='main'>
+            <EditorBox
+                theme={theme}
+                fontSize={fontSize}
+                lang={lang}
+                setUserCode={setUserCode}
+            />
+            <CompileBox
+                stdin={stdin} setStdin={setStdin}
+                stdout={stdout}
+            />
+            </div>
+        </div>
+    );
 }
 
 export default App;
