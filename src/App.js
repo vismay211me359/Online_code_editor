4ad3233d1d893038e2cb1a951068cd7c3c1e6352
vismay211me359 <vismayms10@gmail.com>
2024-10-07 07:22:38 +0530

    connected backend apis to frontend

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-import {useState} from "react";
+import { useState } from "react";
 import EditorBox from './components/EditorBox';
 import CompileBox from './components/CompileBox';
 import Navbar from './components/Navbar';
+import axios from "axios";
 
 function App() {
 
@@ -20,11 +21,35 @@ function App() {
 
     const langCodeList = {
         "javascript": "nodejs",
-        "python": "python3", 
+        "python": "python3",
     };
 
     const handleRun = () => {
+        if (lang === "css" || lang === "html") {
+            alert("cannot run HTML or CSS")
+        } else {
+            if (stdout !== "") {
+                setStdout("");
+            }
 
+            const data = {
+                "stdin": stdin,
+                "script": userCode,
+                "language": langCodeList[lang],
+                "versionId": versionIdList[lang]
+            };
+
+            axios.post("http://localhost:3001/execute", data)
+                .then(function (res) {
+                    console.log(res);
+                    if (res.data.statusCode === 200) {
+                        setStdout(res.data.output)
+                    }
+                })
+                .catch(function (error) {
+                    console.log(error.body)
+                });
+        }
 
     }
 
@@ -38,16 +63,16 @@ function App() {
                 handleRun={handleRun}
             />
             <div className='main'>
-            <EditorBox
-                theme={theme}
-                fontSize={fontSize}
-                lang={lang}
-                setUserCode={setUserCode}
-            />
-            <CompileBox
-                stdin={stdin} setStdin={setStdin}
-                stdout={stdout}
-            />
+                <EditorBox
+                    theme={theme}
+                    fontSize={fontSize}
+                    lang={lang}
+                    setUserCode={setUserCode}
+                />
+                <CompileBox
+                    stdin={stdin} setStdin={setStdin}
+                    stdout={stdout}
+                />
             </div>
         </div>
     );
